e60d9a9d1f631374444e2b86655edca0eeff0a80
Periklis Desyllas <51210446+pedes1999@users.noreply.github.com>
2022-09-26 14:39:33 +0300

    Add files via upload

---
+++ src/pages/Home1.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
+function Home1() {
+  return (
+    <>
+        <UserSearch />
+        <UserResults />
+    </>
+  )
+}
+
+export default Home1
